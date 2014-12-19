e2c25b510e88f081b74267b5820fc8ade99415ab
Kurt Ruppel <me@kurtruppel.com>
2014-12-19 10:24:38 -0800

    Properly prettify the first sample.

--- src/htmln.js
+++ src/htmln.js
@@ -7,29 +7,75 @@
     root.htmln = factory(root.HTML5Tokenizer);
   }
 }(this, function(HTML5Tokenizer) {
+  var INLINE_TAGS = ['title', 'p', 'span', 'button'];
+
+  function indent(size) {
+    var str = '';
+
+    while (size--) {
+      str += ' ';
+    }
+
+    return str;
+  }
+
   return function(html) {
     var textwidth = 80;
+    var indentsize = 2;
 
-    var converted = '',
-        filtered = '',
-        len = html.length,
-        i = 0,
+    var converted = filtered = '',
+        i = indents = 0,
+        len,
         docType,
-        tokens;
+        tokens,
+        token;
 
     filtered = html.replace(/<!doctype[^>]+>\n?/i, function(match) {
       docType = match;
 
       return '';
     });
-    tokens = HTML5Tokenizer.tokenize(html);
+
+    tokens = HTML5Tokenizer.tokenize(filtered);
+    len = tokens.length;
 
     if (docType) {
       converted += docType;
     }
 
-    while (i < len) {
-      converted += html[i++];
+    for (; i < len; i++) {
+      token = tokens[i];
+
+      if (token.type === 'StartTag') {
+        converted += indent(indentsize * indents);
+
+        if (!token.selfClosing) {
+          indents++;
+        }
+
+        converted += '<' + token.tagName;
+
+        if (token.attributes.length) {
+          converted += ' ' + HTML5Tokenizer.Generator.prototype.Attributes(token.attributes);
+        }
+
+        converted += '>';
+
+        if (INLINE_TAGS.indexOf(token.tagName) === -1) {
+          converted += '\n';
+        }
+      } else if (token.type === 'EndTag') {
+        indents--;
+
+        if (INLINE_TAGS.indexOf(token.tagName) === -1) {
+          converted += indent(indentsize * indents);
+        }
+
+        converted += '</' + token.tagName + '>';
+        converted += '\n';
+      } else if (token.type === 'Chars' && token.chars !== '\n') {
+        converted += token.chars;
+      }
     }
 
     return converted;
